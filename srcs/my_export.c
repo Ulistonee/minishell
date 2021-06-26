878dd11067ec72fe3511e2b5c79c792334d973d4
Ulistonee <ulistonee@gmail.com>
2021-06-26 20:17:15 +0300

    working on export

--- srcs/my_export.c
+++ srcs/my_export.c
@@ -3,11 +3,20 @@
 void            my_export(t_all *all)
 {
     pid_t       pid;
+    pid_t       wpid;
+    int         i;
 
     pid = fork();
     if (pid == 0)
     {
 
     }
-
+    else if (pid < 0)
+    {
+        printf("Forking error\n", strerror(errno));
+    }
+    else
+    {
+        wpid = waitpid(pid, &i, WUNTRACED);
+    }
 }
