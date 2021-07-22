de2d393d167fb55f8b5aba896dc7d2dfd5cf7913
Ulistonee <ulistonee@gmail.com>
2021-07-23 00:38:49 +0300

    sa

---
+++ kirill/cmake-build-debug/CMakeFiles/minishell.dir/cmake_clean.cmake
@@ -0,0 +1,24 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/minishell.dir/add_redirect.c.o"
+  "CMakeFiles/minishell.dir/ft_list.c.o"
+  "CMakeFiles/minishell.dir/main.c.o"
+  "CMakeFiles/minishell.dir/parser.c.o"
+  "CMakeFiles/minishell.dir/srcs/another_exit.c.o"
+  "CMakeFiles/minishell.dir/srcs/executor.c.o"
+  "CMakeFiles/minishell.dir/srcs/my_cd.c.o"
+  "CMakeFiles/minishell.dir/srcs/my_echo.c.o"
+  "CMakeFiles/minishell.dir/srcs/my_env.c.o"
+  "CMakeFiles/minishell.dir/srcs/my_export.c.o"
+  "CMakeFiles/minishell.dir/srcs/my_fork.c.o"
+  "CMakeFiles/minishell.dir/srcs/my_pwd.c.o"
+  "CMakeFiles/minishell.dir/srcs/my_unset.c.o"
+  "CMakeFiles/minishell.dir/srcs/utils.c.o"
+  "CMakeFiles/minishell.dir/utils.c.o"
+  "minishell"
+  "minishell.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/minishell.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ kirill/cmake-build-debug/libft/CMakeFiles/libft.dir/DependInfo.cmake
@@ -0,0 +1,63 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/Users/rchalmer/c/minishell/kirill/libft/ft_atoi.c" "/Users/rchalmer/c/minishell/kirill/cmake-build-debug/libft/CMakeFiles/libft.dir/ft_atoi.c.o"
+  "/Users/rchalmer/c/minishell/kirill/libft/ft_bzero.c" "/Users/rchalmer/c/minishell/kirill/cmake-build-debug/libft/CMakeFiles/libft.dir/ft_bzero.c.o"
+  "/Users/rchalmer/c/minishell/kirill/libft/ft_calloc.c" "/Users/rchalmer/c/minishell/kirill/cmake-build-debug/libft/CMakeFiles/libft.dir/ft_calloc.c.o"
+  "/Users/rchalmer/c/minishell/kirill/libft/ft_isalnum.c" "/Users/rchalmer/c/minishell/kirill/cmake-build-debug/libft/CMakeFiles/libft.dir/ft_isalnum.c.o"
+  "/Users/rchalmer/c/minishell/kirill/libft/ft_isalpha.c" "/Users/rchalmer/c/minishell/kirill/cmake-build-debug/libft/CMakeFiles/libft.dir/ft_isalpha.c.o"
+  "/Users/rchalmer/c/minishell/kirill/libft/ft_isascii.c" "/Users/rchalmer/c/minishell/kirill/cmake-build-debug/libft/CMakeFiles/libft.dir/ft_isascii.c.o"
+  "/Users/rchalmer/c/minishell/kirill/libft/ft_isdigit.c" "/Users/rchalmer/c/minishell/kirill/cmake-build-debug/libft/CMakeFiles/libft.dir/ft_isdigit.c.o"
+  "/Users/rchalmer/c/minishell/kirill/libft/ft_isprint.c" "/Users/rchalmer/c/minishell/kirill/cmake-build-debug/libft/CMakeFiles/libft.dir/ft_isprint.c.o"
+  "/Users/rchalmer/c/minishell/kirill/libft/ft_itoa.c" "/Users/rchalmer/c/minishell/kirill/cmake-build-debug/libft/CMakeFiles/libft.dir/ft_itoa.c.o"
+  "/Users/rchalmer/c/minishell/kirill/libft/ft_lstadd_back.c" "/Users/rchalmer/c/minishell/kirill/cmake-build-debug/libft/CMakeFiles/libft.dir/ft_lstadd_back.c.o"
+  "/Users/rchalmer/c/minishell/kirill/libft/ft_lstadd_front.c" "/Users/rchalmer/c/minishell/kirill/cmake-build-debug/libft/CMakeFiles/libft.dir/ft_lstadd_front.c.o"
+  "/Users/rchalmer/c/minishell/kirill/libft/ft_lstclear.c" "/Users/rchalmer/c/minishell/kirill/cmake-build-debug/libft/CMakeFiles/libft.dir/ft_lstclear.c.o"
+  "/Users/rchalmer/c/minishell/kirill/libft/ft_lstdelone.c" "/Users/rchalmer/c/minishell/kirill/cmake-build-debug/libft/CMakeFiles/libft.dir/ft_lstdelone.c.o"
+  "/Users/rchalmer/c/minishell/kirill/libft/ft_lstiter.c" "/Users/rchalmer/c/minishell/kirill/cmake-build-debug/libft/CMakeFiles/libft.dir/ft_lstiter.c.o"
+  "/Users/rchalmer/c/minishell/kirill/libft/ft_lstlast.c" "/Users/rchalmer/c/minishell/kirill/cmake-build-debug/libft/CMakeFiles/libft.dir/ft_lstlast.c.o"
+  "/Users/rchalmer/c/minishell/kirill/libft/ft_lstmap.c" "/Users/rchalmer/c/minishell/kirill/cmake-build-debug/libft/CMakeFiles/libft.dir/ft_lstmap.c.o"
+  "/Users/rchalmer/c/minishell/kirill/libft/ft_lstnew.c" "/Users/rchalmer/c/minishell/kirill/cmake-build-debug/libft/CMakeFiles/libft.dir/ft_lstnew.c.o"
+  "/Users/rchalmer/c/minishell/kirill/libft/ft_lstsize.c" "/Users/rchalmer/c/minishell/kirill/cmake-build-debug/libft/CMakeFiles/libft.dir/ft_lstsize.c.o"
+  "/Users/rchalmer/c/minishell/kirill/libft/ft_memccpy.c" "/Users/rchalmer/c/minishell/kirill/cmake-build-debug/libft/CMakeFiles/libft.dir/ft_memccpy.c.o"
+  "/Users/rchalmer/c/minishell/kirill/libft/ft_memchr.c" "/Users/rchalmer/c/minishell/kirill/cmake-build-debug/libft/CMakeFiles/libft.dir/ft_memchr.c.o"
+  "/Users/rchalmer/c/minishell/kirill/libft/ft_memcmp.c" "/Users/rchalmer/c/minishell/kirill/cmake-build-debug/libft/CMakeFiles/libft.dir/ft_memcmp.c.o"
+  "/Users/rchalmer/c/minishell/kirill/libft/ft_memcpy.c" "/Users/rchalmer/c/minishell/kirill/cmake-build-debug/libft/CMakeFiles/libft.dir/ft_memcpy.c.o"
+  "/Users/rchalmer/c/minishell/kirill/libft/ft_memmove.c" "/Users/rchalmer/c/minishell/kirill/cmake-build-debug/libft/CMakeFiles/libft.dir/ft_memmove.c.o"
+  "/Users/rchalmer/c/minishell/kirill/libft/ft_memset.c" "/Users/rchalmer/c/minishell/kirill/cmake-build-debug/libft/CMakeFiles/libft.dir/ft_memset.c.o"
+  "/Users/rchalmer/c/minishell/kirill/libft/ft_putchar_fd.c" "/Users/rchalmer/c/minishell/kirill/cmake-build-debug/libft/CMakeFiles/libft.dir/ft_putchar_fd.c.o"
+  "/Users/rchalmer/c/minishell/kirill/libft/ft_putendl_fd.c" "/Users/rchalmer/c/minishell/kirill/cmake-build-debug/libft/CMakeFiles/libft.dir/ft_putendl_fd.c.o"
+  "/Users/rchalmer/c/minishell/kirill/libft/ft_putnbr_fd.c" "/Users/rchalmer/c/minishell/kirill/cmake-build-debug/libft/CMakeFiles/libft.dir/ft_putnbr_fd.c.o"
+  "/Users/rchalmer/c/minishell/kirill/libft/ft_putstr_fd.c" "/Users/rchalmer/c/minishell/kirill/cmake-build-debug/libft/CMakeFiles/libft.dir/ft_putstr_fd.c.o"
+  "/Users/rchalmer/c/minishell/kirill/libft/ft_split.c" "/Users/rchalmer/c/minishell/kirill/cmake-build-debug/libft/CMakeFiles/libft.dir/ft_split.c.o"
+  "/Users/rchalmer/c/minishell/kirill/libft/ft_strchr.c" "/Users/rchalmer/c/minishell/kirill/cmake-build-debug/libft/CMakeFiles/libft.dir/ft_strchr.c.o"
+  "/Users/rchalmer/c/minishell/kirill/libft/ft_strdup.c" "/Users/rchalmer/c/minishell/kirill/cmake-build-debug/libft/CMakeFiles/libft.dir/ft_strdup.c.o"
+  "/Users/rchalmer/c/minishell/kirill/libft/ft_strjoin.c" "/Users/rchalmer/c/minishell/kirill/cmake-build-debug/libft/CMakeFiles/libft.dir/ft_strjoin.c.o"
+  "/Users/rchalmer/c/minishell/kirill/libft/ft_strlcat.c" "/Users/rchalmer/c/minishell/kirill/cmake-build-debug/libft/CMakeFiles/libft.dir/ft_strlcat.c.o"
+  "/Users/rchalmer/c/minishell/kirill/libft/ft_strlcpy.c" "/Users/rchalmer/c/minishell/kirill/cmake-build-debug/libft/CMakeFiles/libft.dir/ft_strlcpy.c.o"
+  "/Users/rchalmer/c/minishell/kirill/libft/ft_strlen.c" "/Users/rchalmer/c/minishell/kirill/cmake-build-debug/libft/CMakeFiles/libft.dir/ft_strlen.c.o"
+  "/Users/rchalmer/c/minishell/kirill/libft/ft_strmapi.c" "/Users/rchalmer/c/minishell/kirill/cmake-build-debug/libft/CMakeFiles/libft.dir/ft_strmapi.c.o"
+  "/Users/rchalmer/c/minishell/kirill/libft/ft_strncmp.c" "/Users/rchalmer/c/minishell/kirill/cmake-build-debug/libft/CMakeFiles/libft.dir/ft_strncmp.c.o"
+  "/Users/rchalmer/c/minishell/kirill/libft/ft_strnstr.c" "/Users/rchalmer/c/minishell/kirill/cmake-build-debug/libft/CMakeFiles/libft.dir/ft_strnstr.c.o"
+  "/Users/rchalmer/c/minishell/kirill/libft/ft_strrchr.c" "/Users/rchalmer/c/minishell/kirill/cmake-build-debug/libft/CMakeFiles/libft.dir/ft_strrchr.c.o"
+  "/Users/rchalmer/c/minishell/kirill/libft/ft_strtrim.c" "/Users/rchalmer/c/minishell/kirill/cmake-build-debug/libft/CMakeFiles/libft.dir/ft_strtrim.c.o"
+  "/Users/rchalmer/c/minishell/kirill/libft/ft_substr.c" "/Users/rchalmer/c/minishell/kirill/cmake-build-debug/libft/CMakeFiles/libft.dir/ft_substr.c.o"
+  "/Users/rchalmer/c/minishell/kirill/libft/ft_tolower.c" "/Users/rchalmer/c/minishell/kirill/cmake-build-debug/libft/CMakeFiles/libft.dir/ft_tolower.c.o"
+  "/Users/rchalmer/c/minishell/kirill/libft/ft_toupper.c" "/Users/rchalmer/c/minishell/kirill/cmake-build-debug/libft/CMakeFiles/libft.dir/ft_toupper.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "/Users/rchalmer/.brew/opt/readline/lib"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
