126799938e66b13c27d843b8b49ad00cd905641b
Ulistonee <ulistonee@gmail.com>
2021-07-30 15:18:13 +0300

    final

---
+++ kirill/cmake-build-debug/libft/CMakeFiles/libft.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/libft.dir/ft_atoi.c.o"
+  "CMakeFiles/libft.dir/ft_bzero.c.o"
+  "CMakeFiles/libft.dir/ft_calloc.c.o"
+  "CMakeFiles/libft.dir/ft_isalnum.c.o"
+  "CMakeFiles/libft.dir/ft_isalpha.c.o"
+  "CMakeFiles/libft.dir/ft_isdigit.c.o"
+  "CMakeFiles/libft.dir/ft_itoa.c.o"
+  "CMakeFiles/libft.dir/ft_putchar_fd.c.o"
+  "CMakeFiles/libft.dir/ft_putendl_fd.c.o"
+  "CMakeFiles/libft.dir/ft_putnbr_fd.c.o"
+  "CMakeFiles/libft.dir/ft_putstr_fd.c.o"
+  "CMakeFiles/libft.dir/ft_split.c.o"
+  "CMakeFiles/libft.dir/ft_strchr.c.o"
+  "CMakeFiles/libft.dir/ft_strdup.c.o"
+  "CMakeFiles/libft.dir/ft_strjoin.c.o"
+  "CMakeFiles/libft.dir/ft_strlen.c.o"
+  "CMakeFiles/libft.dir/ft_strncmp.c.o"
+  "CMakeFiles/libft.dir/ft_substr.c.o"
+  "liblibft.a"
+  "liblibft.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/libft.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
